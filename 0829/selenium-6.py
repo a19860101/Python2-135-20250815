c2a75631d1490f162edf288be1f768b7a6d57bf8
Zac <a19860101@gmail.com>
2025-08-29 19:39:01 +0800

    20250829

--- 0829/selenium-6.py
+++ 0829/selenium-6.py
@@ -7,19 +7,29 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
-url = 'https://www.nike.com/tw/w/mens-training-gym-shoes-58jtoznik1zy7ok'
+url = 'https://www.nike.com/tw/w/mens-basketball-shoes-3glsmznik1zy7ok'
 driver.get(url)
 driver.maximize_window()
 # loader-bar
-
+pcount = 0
 while True:
-    driver.execute_script('window.scrollTo(0, document.body.scrollHeight-1000)')
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight-1200)')
 
     try:
         WebDriverWait(driver, 10).until(
-            # EC.invisibility_of_element_located(By.CLASS_NAME, 'loader-bar')
-            EC.invisibility_of_element((By.CLASS_NAME, 'loader-bar'))
+            EC.invisibility_of_element_located((By.CLASS_NAME, 'loader-bar'))
+            # EC.invisibility_of_element((By.CLASS_NAME, 'loader-bar'))
         )
     except Exception as e:
         print(e)
-        driver.close()
+
+    products = driver.find_elements(By.CLASS_NAME, 'product-card')
+    if pcount == len(products):
+        break
+    pcount = len(products)
+
+time.sleep(5)
+print(pcount)
+for product in products:
+    print(product.find_element(By.CLASS_NAME, 'product-card__title').text)
+driver.close()
