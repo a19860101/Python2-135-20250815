bb018164400d155baadc122b4d2f0b47e19e6732
Zac <a19860101@gmail.com>
2025-08-29 21:13:33 +0800

    20250829

--- 0829/selenium-uniqlo.py
+++ 0829/selenium-uniqlo.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -16,13 +17,16 @@ search.click()
 
 time.sleep(3)
 
-men = driver.find_element(By.XPATH, '//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[1]/span[2]')
-men.click()
+# men = driver.find_element(By.XPATH, '//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[1]/span[2]')
+# men.click()
 
-time.sleep(3)
+# time.sleep(3)
 
 # rank = driver.find_element(By.XPATH,'//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[1]/div/div/div[27]/div/span')
-rank = driver.find_element(By.XPATH,'//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[2]/div/div/div[26]/div/span')
-rank.click()
+# rank = driver.find_element(By.XPATH,'//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[2]/div/div/div[26]/div/span')
+# rank.click()
 
+# t = driver.find_element(By.XPATH, '//*[@id="hmall-container"]/div/div[1]/div[3]/div[4]/div[1]/div/div/input')
+t = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="請輸入關鍵字"]')
+t.send_keys('外套',Keys.ENTER)
 time.sleep(20)
