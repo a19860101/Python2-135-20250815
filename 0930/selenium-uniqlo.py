75ade4bfb83a4b45c716098aa62a8706b2895562
Zac <a19860101@gmail.com>
2025-09-01 20:02:11 +0800

    20250901

---
+++ 0930/selenium-uniqlo.py
@@ -0,0 +1,61 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome()
+
+url = 'https://www.uniqlo.com/tw/zh_TW/'
+driver.get(url)
+driver.maximize_window()
+
+search = driver.find_element(By.CLASS_NAME,'li-search')
+search.click()
+
+time.sleep(3)
+
+# men = driver.find_element(By.XPATH, '//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[1]/span[2]')
+# men.click()
+
+# time.sleep(3)
+
+newproduct = driver.find_element(By.CSS_SELECTOR, 'img[alt="新品上市"]')
+newproduct.click()
+
+# time.sleep(5)
+
+# rank = driver.find_element(By.XPATH,'//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[1]/div/div/div[27]/div/span')
+# rank = driver.find_element(By.XPATH,'//*[@id="hmall-container"]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[2]/div/div/div[26]/div/span')
+# rank.click()
+
+# t = driver.find_element(By.XPATH, '//*[@id="hmall-container"]/div/div[1]/div[3]/div[4]/div[1]/div/div/input')
+# t = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="請輸入關鍵字"]')
+# t.send_keys('外套',Keys.ENTER)
+# time.sleep(10)
+WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.CLASS_NAME,'h-product-group'))
+)
+
+
+pcount = 0
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    time.sleep(5)
+    # loading-paging
+
+    # try:
+    #     WebDriverWait(driver, 10).until(
+    #         EC.invisibility_of_element_located((By.CLASS_NAME, 'loading-paging'))
+    #     )
+    # except Exception as e:
+    #     print(e)
+
+    products = driver.find_elements(By.CLASS_NAME, 'ec-font-sub-title')
+    if pcount == len(products):
+        break
+    pcount = len(products)
+
+print(pcount)
